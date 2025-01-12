9aa4a5bc13ad8782a993663d65c2638d4e4896ca
Avijitbera <avijit.bera@fitnearn.com>
2025-01-12 20:14:11 +0530

    create table class for base table

--- example/basesync_example.dart
+++ example/basesync_example.dart
@@ -1,6 +1,1 @@
-import 'package:basesync/basesync.dart';
 
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
-}
---
+++ lib/src/core/Table.dart
@@ -0,0 +1,21 @@
+
+abstract class Table {
+  int get id;
+
+  /// Registry for factory methods
+  static final Map<Type, Table Function(Map<String, dynamic>)> _factoryRegistry = {};
+
+  Map<String , dynamic> toMap();
+
+  static Table fromMap(Type tableType, Map<String , dynamic> map) {
+    var factory = _factoryRegistry[tableType];
+    if(factory != null){
+      return factory(map);
+    }
+    throw Exception("Table $tableType is not registered");
+  }
+
+  static void registerFactory(Type tableType, Table Function(Map<String, dynamic>) factory) {
+    _factoryRegistry[tableType] = factory;
+  }
+}
